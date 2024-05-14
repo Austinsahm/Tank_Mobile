6f1f142aeea2b7b947802d44806cec9dd427cce8
Oladeji Oluwamayowa Samuel <87494596+Austinsahm@users.noreply.github.com>
2024-05-14 22:41:49 +0100

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiServerEndpoint: 'https://test.datanucleusinc.com/greensky393',
-  googleMapsApi: "AIzaSyAV5nlpyikzoUG3oBNM3RJ4ZHcZpif8n9M",
-};
